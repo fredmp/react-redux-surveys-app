9c04f74f0e9b89898aaa00fcb39269087492a853
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-08 19:55:20 -0200

    Add dotenv and passport with google oauth2 strategy

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
 const express = require('express');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
 
+const { PORT, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'testing deploy' });
-});
+passport.use(
+  new Strategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+      done();
+    },
+  ),
+);
 
-const { PORT } = process.env;
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT || 5000);
