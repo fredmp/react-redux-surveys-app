8264bd8976c6027cb310ba43ffe35e6b4b444e62
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-10 17:05:57 -0200

    Add cookie session lib and new routes

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models');
 require('./services/passport');
 
-const { PORT, MONGO_URI } = process.env;
+const { PORT, MONGO_URI, COOKIE_KEY } = process.env;
 
 mongoose.connect(MONGO_URI);
 
 const app = express();
+
+const maxAge = 30 * 24 * 60 * 60 * 1000; // 30 days
+
+app.use(
+  cookieSession({
+    maxAge,
+    keys: [COOKIE_KEY],
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes')(app);
 
